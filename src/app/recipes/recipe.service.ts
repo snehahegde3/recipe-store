5d105c8676bc867985fdb58c81150c3abeab2948
Sneha Hegde <snehahegde33@gmail.com>
2023-03-11 18:38:33 +0530

    ingredients to shopping list functionality added

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from "@angular/core";
+import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
+
+@Injectable()
 export class RecipeService{ 
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
@@ -16,9 +19,20 @@ export class RecipeService{
           'https://images.unsplash.com/photo-1528498033373-3c6c08e93d79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80', 
           [new Ingredient('Cilantro', 2), new Ingredient('Kiwi', 3), new Ingredient('Mint', 1), new Ingredient('Herbs', 1)]
         ),
+        new Recipe(
+          'Fried egg', 
+          'Perfectly fried egg placed on wonderfully roasted gplden bread, seasoned just right', 
+          'https://images.unsplash.com/photo-1562918005-50afb98e5d32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+          [new Ingredient('Egg', 2), new Ingredient('Pepper', 2), new Ingredient('Salt', 1), new Ingredient('Cilantro', 3), new Ingredient('Bread', 2)]
+        )
       ];
     
+    constructor(private slService: ShoppingListService){}
     getRecipes(){ 
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+      this.slService.addIngredients(ingredients);
     }
 }
