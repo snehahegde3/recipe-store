2f7d283eb86b30d65280775829a44ab77ba01948
Sneha Hegde <snehahegde33@gmail.com>
2023-03-11 17:25:04 +0530

    added an array of ingredients in the recipe model

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 export class RecipeService{ 
     recipeSelected = new EventEmitter<Recipe>();
@@ -6,12 +7,14 @@ export class RecipeService{
         new Recipe(
           'Egg salad',
           'Egg salad with avacado, fig',
-          'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80'
+          'https://images.unsplash.com/photo-1490645935967-10de6ba17061?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=853&q=80', 
+          [new Ingredient('Avacado', 2), new Ingredient('Eggs', 2), new Ingredient('Fig', 1)]
         ),
         new Recipe(
           'Cilantro kiwi smoothie',
           'Cilantro, kiwi, mint and some of your favourite herbs smoothied',
-          'https://images.unsplash.com/photo-1528498033373-3c6c08e93d79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80'
+          'https://images.unsplash.com/photo-1528498033373-3c6c08e93d79?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=385&q=80', 
+          [new Ingredient('Cilantro', 2), new Ingredient('Kiwi', 3), new Ingredient('Mint', 1), new Ingredient('Herbs', 1)]
         ),
       ];
     
