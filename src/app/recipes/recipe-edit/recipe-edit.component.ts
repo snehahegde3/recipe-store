d8d3ef6cb023200071b027a5f77db98eb70bf768
Sneha Hegde <snehahegde33@gmail.com>
2023-04-12 08:39:16 +0530

    recipe edit init commit

---
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-edit',
+  templateUrl: './recipe-edit.component.html',
+  styleUrls: ['./recipe-edit.component.css']
+})
+export class RecipeEditComponent implements OnInit {
+
+  ngOnInit(): void {
+
+  }
+}
