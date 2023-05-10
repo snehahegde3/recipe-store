5b6474df23b539063f07b3756dbb0a0b8f86674d
Sneha Hegde <snehahegde33@gmail.com>
2023-05-10 09:16:55 +0530

    added cleanup for shopping-list

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
 import { EventEmitter } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
-import { Subject } from "rxjs";
+import { Subject, Subscription } from "rxjs";
 
 export class ShoppingListService {
     ingredientsChanged = new Subject<Ingredient[]>();
+
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
